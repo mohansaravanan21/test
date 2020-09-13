043d1eb17b7750466fc45de66fefe71b5493a441
mohansaravanan21 <48146661+mohansaravanan21@users.noreply.github.com>
2020-09-13 17:37:09 +0530

    added y
    Y is added

--- hungry.py
+++ hungry.py
@@ -1,6 +1,6 @@
 hungry = input("Are you Hungry??")
 
-if hungry=="yes" or hungry=="yup":
+if hungry=="yes" or hungry=="yup" or hungry=="Y":
     print("well, eat something...")
     print("pizza")
     print("burger")
